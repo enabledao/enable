e64001f3c264c0ebd1f61290423c013142128585
tspoff@gmail.com <tspoff@gmail.com>
2019-04-23 17:46:44 -0700

    add basic user staking

---
+++ contracts/UserStaking.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/* 
+Permanent on-chain record of who staked who. 
+Uses events which are indexed off-chain.
+*/
+contract UserStaking is Ownable {
+
+  constructor() Ownable() public {}
+
+  event UserStaked(address indexed sender, address indexed recipient);
+
+  function stakeUser(address _recipient) public {
+      emit UserStaked(msg.sender, _recipient);
+  }
+
+}
