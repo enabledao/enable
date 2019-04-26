cd60d3cccf806d2160522b12f06d52ce6f24c380
tspoff@gmail.com <tspoff@gmail.com>
2019-04-26 10:55:48 -0700

    Only bloom users can stake

--- contracts/UserStaking.sol
+++ contracts/UserStaking.sol
@@ -1,17 +1,29 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-
+import "./bloominterface/IAccountRegistryLogic.sol";
 /* 
-Permanent on-chain record of who staked who. 
+Permanent on-chain record of who staked (vouched for the creditworthiness of) whom.
 Uses events which are indexed off-chain.
 */
 contract UserStaking is Ownable {
-    event UserStaked(address indexed sender, address indexed recipient);
+    IAccountRegistryLogic accountRegistryLogic;
 
-    constructor() Ownable() public {}
+    event UserStaked(address indexed sender, address indexed recipient);
+    event AccountRegistryLogicAddressChanged(address newAddress);
+
+    constructor(address _accountRegistryLogicAddress) Ownable() public {
+        accountRegistryLogic = IAccountRegistryLogic(_accountRegistryLogicAddress);
+    }
 
     function stakeUser(address _recipient) public {
+        require (accountRegistryLogic.linkIds(_recipient) != 0, "Recipient must be a member of Bloom network");
+        require (accountRegistryLogic.linkIds(msg.sender) != 0, "Sender must be a member of Bloom network");
         emit UserStaked(msg.sender, _recipient);
+    }
+
+    function setAccountRegistryLogicAddress(address _newAddress) onlyOwner() {
+        accountRegistryLogic = IAccountRegistryLogic(_newAddress);
+        emit AccountRegistryLogicAddressChanged(_newAddress);
     }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -17,12 +17,14 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
+require('dotenv').config()
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = process.env.INFURA_KEY;
+
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = process.env.WALLET_MNEMONIC;
 
 module.exports = {
   /**
@@ -60,14 +62,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {
