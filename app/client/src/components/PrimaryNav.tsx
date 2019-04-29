487ee0e3923b33fa46ad8d632e101847d8e5cb12
Daniel <dan@daniel.ai>
2019-04-30 01:21:30 +0800

    Fix NavBar and add react-shards-dashboard CSS

--- app/client/src/components/PrimaryNav.tsx
+++ app/client/src/components/PrimaryNav.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 import {
+  Container,
   Button,
   Dropdown,
   DropdownToggle,
@@ -14,15 +16,16 @@ import {
   NavLink,
   Collapse
 } from "shards-react";
-import { Heading } from 'rimble-ui'
+import { Heading } from "rimble-ui";
+import logo from "../assets/logo.png";
 
-interface MyState  {
+interface MyState {
   collapseOpen: boolean;
   dropdownOpen: boolean;
-};
+}
 
 export class PrimaryNav extends React.Component<{}, MyState> {
-  state: MyState
+  state: MyState;
 
   constructor(props) {
     super(props);
@@ -55,43 +58,81 @@ export class PrimaryNav extends React.Component<{}, MyState> {
   }
 
   render() {
+    const classes = classNames("main-navbar", "bg-white", "sticky-top");
+
     return (
-      <Navbar type="dark" expand="md" style={{ background: "transparent", borderBottom: "solid 5px rgba(0,0,0,0.1)", padding: 0 }}>
-        <NavbarBrand href="/">
-          <Heading.h4>
-            Enable
-          </Heading.h4>
-        </NavbarBrand>
-        <NavbarToggler onClick={this.toggleNavbar} />
-
-        <Collapse open={this.state.collapseOpen} navbar>
-          <Nav navbar>
-            <Dropdown
-              open={this.state.dropdownOpen}
-              toggle={this.toggleDropdown}
+      <div className={classes}>
+        <Container className="p-0">
+          <Navbar
+            type="light"
+            className="align-items-stretch ml-2 flex-md-nowrap p-0"
+          >
+            <NavbarBrand
+              className="w-100 mr-0"
+              href="#"
+              style={{ lineHeight: "25px" }}
             >
-              <DropdownToggle nav caret>
-                Loans
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem><Link to="/loan/Education">Education</Link></DropdownItem>
-                <DropdownItem><Link>Agriculture</Link></DropdownItem>
-                <DropdownItem><Link>Mission-driven Orgs</Link></DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </Nav>
+              <img
+                id="main-logo"
+                className="d-inline-block align-top mr-2"
+                src={logo}
+                alt=""
+                height="32"
+              />
+              <span className="md-inline ml-1">
+                <h5
+                  className="app__brand-name"
+                  // style={{
+                  //   lineHeight: "34px"
+                  // }}
+                >
+                  Enable
+                </h5>
+              </span>
+            </NavbarBrand>
+            <Nav navbar className="ml-auto app__navbar-button">
+              <NavItem>
+                <Link to="/dashboard">
+                  <Button theme="primary">Dashboard</Button>
+                </Link>
+              </NavItem>
+            </Nav>
+            {/* <NavbarToggler onClick={this.toggleNavbar} /> */}
 
-          <Nav navbar className="ml-auto">
-            <NavItem>
-              <Link to="/dashboard">
-                <Button theme="primary">
-                  Dashboard
-                </Button>
-              </Link>
-            </NavItem>          
-          </Nav>
-        </Collapse>
-      </Navbar>
+            {/* <Collapse open={this.state.collapseOpen} navbar>
+              <Nav navbar>
+                <Dropdown
+                  open={this.state.dropdownOpen}
+                  toggle={this.toggleDropdown}
+                >
+                  <DropdownToggle nav caret>
+                    Loans
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem>
+                      <Link to="/loan/Education">Education</Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <Link>Agriculture</Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <Link>Mission-driven Orgs</Link>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </Nav>
+
+              <Nav navbar className="ml-auto">
+                <NavItem>
+                  <Link to="/dashboard">
+                    <Button theme="primary">Dashboard</Button>
+                  </Link>
+                </NavItem>
+              </Nav>
+            </Collapse> */}
+          </Navbar>
+        </Container>
+      </div>
     );
   }
 }
