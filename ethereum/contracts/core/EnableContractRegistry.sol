1555ec1f47a16d8f5d787e6cde29655790489ad1
tspoff@gmail.com <tspoff@gmail.com>
2019-05-28 14:23:22 -0400

    deploy logic

--- ethereum/contracts/core/EnableContractRegistry.sol
+++ ethereum/contracts/core/EnableContractRegistry.sol
@@ -13,6 +13,23 @@ import "../loans/student-loans/StudentLoanTermsStorage.sol";
 
 // @notice Registry for Enable core contract addresses. Modelled after Dharma ContractRegistry.
 contract EnableContractRegistry is Ownable, IEnableContractRegistry {
+     // System Core
+    PermissionsLib public permissionsLib;
+    UserStaking public userStaking;
+    
+    // Loans Core
+    DebtTokenFactory public debtTokenFactory;
+    
+    // Student Loans
+    StudentLoanCrowdfundFactory public studentLoanCrowdfundFactory;
+    StudentLoanTermsStorage public studentLoanTermsStorage;
+
+    bool public initialized;
+
+    event ContractAddressInitialized(
+        ContractType indexed contractType,
+        address indexed newAddress
+    );
 
     event ContractAddressUpdated(
         ContractType indexed contractType,
@@ -22,34 +39,38 @@ contract EnableContractRegistry is Ownable, IEnableContractRegistry {
 
     enum ContractType {
         PermissionsLib,
-        StudentLoanCrowdfundFactory
+        UserStaking,
+        DebtTokenFactory,
+        StudentLoanCrowdfundFactory,
+        StudentLoanTermsStorage
     }
-    
-    // System Core
-    PermissionsLib public permissionsLib;
-    
-    // Loans Core
-    DebtTokenFactory public debtTokenFactory;
-    
-    // Student Loans
-    StudentLoanCrowdfundFactory public studentLoanCrowdfundFactory;
-    // StudentLoanTermsContract public studentLoanTermsContract;
-    StudentLoanTermsStorage public studentLoanTermsStorage;
-
-    constructor(
+   
+    function initialize(
         address _permissionsLib,
+        address _userStaking,
         address _debtTokenFactory,
         address _studentLoanCrowdfundFactory,
-        // address _studentLoanTermsContract,
         address _studentLoanTermsStorage
-    ) public {
-        permissionsLib = PermissionsLib(_permissionsLib);
-        
-        debtTokenFactory = DebtTokenFactory(_debtTokenFactory);
-        
-        studentLoanCrowdfundFactory = StudentLoanCrowdfundFactory(_studentLoanCrowdfundFactory);
-        // studentLoanTermsContract = StudentLoanTermsContract(_studentLoanTermsContract);
-        studentLoanTermsStorage = StudentLoanTermsStorage(_studentLoanTermsStorage);
+    ) public onlyOwner {
+            require(!initialized, "ALREADY_INITIALIZED");
+
+            permissionsLib = PermissionsLib(_permissionsLib);
+            userStaking = UserStaking(_userStaking);
+
+            debtTokenFactory = DebtTokenFactory(_debtTokenFactory);
+
+            studentLoanCrowdfundFactory = StudentLoanCrowdfundFactory(_studentLoanCrowdfundFactory);
+            studentLoanTermsStorage = StudentLoanTermsStorage(_studentLoanTermsStorage);
+
+            emit ContractAddressInitialized(ContractType.PermissionsLib, _permissionsLib);
+            emit ContractAddressInitialized(ContractType.UserStaking, _userStaking);
+
+            emit ContractAddressInitialized(ContractType.DebtTokenFactory, _debtTokenFactory);
+
+            emit ContractAddressInitialized(ContractType.StudentLoanCrowdfundFactory, _studentLoanCrowdfundFactory);
+            emit ContractAddressInitialized(ContractType.StudentLoanTermsStorage, _studentLoanTermsStorage);
+
+            initialized = true;
     }
 
     //TODO: Decide what's updatable and add here.
@@ -60,6 +81,21 @@ contract EnableContractRegistry is Ownable, IEnableContractRegistry {
             oldAddress = address(permissionsLib);
             _validateNewAddress(newAddress, oldAddress);
             permissionsLib = PermissionsLib(newAddress);
+        } else if (contractType == ContractType.UserStaking) {
+            _validateNewAddress(newAddress, address(userStaking));
+            userStaking = UserStaking(newAddress);
+        } else if (contractType == ContractType.DebtTokenFactory) {
+            oldAddress = address(debtTokenFactory);
+            _validateNewAddress(newAddress, oldAddress);
+            debtTokenFactory = DebtTokenFactory(newAddress);
+        } else if (contractType == ContractType.StudentLoanCrowdfundFactory) {
+            oldAddress = address(studentLoanCrowdfundFactory);
+            _validateNewAddress(newAddress, oldAddress);
+            studentLoanCrowdfundFactory = StudentLoanCrowdfundFactory(newAddress);
+        } else if (contractType == ContractType.StudentLoanTermsStorage) {
+            oldAddress = address(studentLoanTermsStorage);
+            _validateNewAddress(newAddress, oldAddress);
+            studentLoanTermsStorage = StudentLoanTermsStorage(newAddress);
         } else {
             revert();
         }
--- ethereum/migrations/1_initial_migration.js
+++ ethereum/migrations/1_initial_migration.js
@@ -1,9 +1,39 @@
 const Migrations = artifacts.require("Migrations");
-const UserStaking = artifacts.require("UserStaking");
-const LoanRequestFactory = artifacts.require("LoanRequestFactory");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(UserStaking);
-  deployer.deploy(LoanRequestFactory);
+// System Core
+const UserStaking = artifacts.require("UserStaking");
+const PermissionsLib = artifacts.require("PermissionsLib");
+const EnableContractRegistry = artifacts.require("EnableContractRegistry");
+
+// Loans Core
+const DebtTokenFactory = artifacts.require("DebtTokenFactory");
+
+// Student Loans
+const StudentLoanTermsContract = artifacts.require("StudentLoanTermsContract");
+const StudentLoanTermsStorage = artifacts.require("StudentLoanTermsStorage");
+const StudentLoanCrowdfundFactory = artifacts.require("StudentLoanCrowdfundFactory");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  const enableRegistry = await deployer.deploy(EnableContractRegistry);
+  console.log(enableRegistry.address);
+
+  const userStaking = await deployer.deploy(UserStaking);
+  const permissionsLib = await deployer.deploy(PermissionsLib);
+
+  const debtTokenFactory = await deployer.deploy(DebtTokenFactory, enableRegistry.address);
+  // const studentLoanTermsContract = await deployer.deploy(StudentLoanTermsContract, enableRegistry.address);
+  const studentLoanTermsStorage = await deployer.deploy(StudentLoanTermsStorage, enableRegistry.address);
+  const studentLoanCrowdfundFactory = await deployer.deploy(StudentLoanCrowdfundFactory, enableRegistry.address);
+
+  await enableRegistry.initialize(
+    permissionsLib.address,
+    userStaking.address,
+    debtTokenFactory.address,
+    studentLoanCrowdfundFactory.address,
+    studentLoanTermsStorage.address,
+  );
+
+  const result = await enableRegistry.permissionsLib();
+  console.log(result);
 };
