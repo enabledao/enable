31aeb5ad6f41a232d72f07b68e78f14d0b40740f
adibas03 <adibas03@ymail.com>
2019-06-03 22:49:00 +0100

    init

---
+++ ethereum/test/end_to_end.js
@@ -0,0 +1,45 @@
+// System Core
+const UserStaking = artifacts.require("UserStaking");
+const PermissionsLib = artifacts.require("PermissionsLib");
+const EnableContractRegistry = artifacts.require("EnableContractRegistry");
+
+// Loans Core
+const DebtTokenFactory = artifacts.require("DebtTokenFactory");
+
+// Student Loans
+const StudentLoanTermsContract = artifacts.require("StudentLoanTermsContract");
+const StudentLoanTermsStorage = artifacts.require("StudentLoanTermsStorage");
+const StudentLoanCrowdfundFactory = artifacts.require("StudentLoanCrowdfundFactory");
+
+let userStaking;
+let permissionsLib;
+let enableContractRegistry;
+let debtTokenFactory;
+let studentLoanTermsContract;
+let studentLoanTermsStorage;
+let studentLoanCrowdfundFactory;
+
+contract( 'ALL', function (accounts) {
+  before(async function () {
+    userStaking = await UserStaking.deployed();
+    permissionsLib = await PermissionsLib.deployed();
+    enableContractRegistry = await EnableContractRegistry.deployed();
+    debtTokenFactory = await DebtTokenFactory.deployed();
+    studentLoanTermsContract = await StudentLoanTermsContract.deployed();
+    studentLoanTermsStorage = await StudentLoanTermsStorage.deployed();
+    studentLoanCrowdfundFactory =  await StudentLoanCrowdfundFactory.deployed();
+
+    assert.exists(userStaking.address, "Failed to deploy UserStaking with address");
+    assert.exists(permissionsLib.address, "Failed to deploy PermissionsLib with address");
+    assert.exists(enableContractRegistry.address, "Failed to deploy EnableContractRegistry with address");
+    assert.exists(debtTokenFactory.address, "Failed to deploy DebtTokenFactory with address");
+    assert.exists(studentLoanTermsContract.address, "Failed to deploy StudentLoanTermsContract with address");
+    assert.exists(studentLoanTermsStorage.address, "Failed to deploy StudentLoanTermsStorage with address");
+    assert.exists(studentLoanCrowdfundFactory.address, "Failed to deploy StudentLoanCrowdfundFactory with address");
+  });
+
+  describe("Test", async function () {
+      console.log("Test Describe")
+  })
+
+});
