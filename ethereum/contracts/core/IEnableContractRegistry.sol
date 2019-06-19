3619d323c9b6b01d727681cbd0b2885435d3277b
Daniel <dan@daniel.ai>
2019-06-19 11:36:44 +0200

    Remove contracts folders

--- ethereum/contracts/core/IEnableContractRegistry.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.2;
-
-// @notice Registry for Enable core contract addresses. Modelled after Dharma ContractRegistry.
-interface IEnableContractRegistry {
-
-    function permissionsLib() external returns(address);
-    function studentLoanCrowdfundFactory() external returns(address);
-}
--- ethereum/contracts/core/PermissionsLib.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.4.21 <0.6.0;
-
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-
-// @notice Code for general permission managment, and common code such as onlyBloomUser.
-contract PermissionsLib is Ownable {}
