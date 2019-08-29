9ba36251a0558d6227afdc9820ba2d2a5b7cef72
Felix <felix.yuniar@gmail.com>
2019-08-29 17:00:54 +0700

    Initial Sketch and Framer Upload

---
+++ design/framer_files/InesFund_mobile1.framerfx/code/canvas.tsx
@@ -0,0 +1,22 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+
+export const colors = Object.freeze({
+    /** #363BD3 */
+    "Enable Blue": "var(--token-c1ea6f7c-d13b-4f4c-b559-449711dea794, rgb(54, 59, 211))",
+    /** #272B90 */
+    "Enable Dark Blue": "var(--token-5854cecc-df6a-41f4-8810-2d93d3d5a490, rgb(39, 43, 144))",
+    /** #21B549 */
+    "Enable Green": "var(--token-97d6cdea-b672-4647-8d92-6a74b9fe3423, rgb(33, 181, 73))",
+    /** #161B31 */
+    "TextBlue": "var(--token-510fc827-78f8-4a81-8ee8-82a09c674a44, rgb(22, 27, 49))",
+    /** #474C62 */
+    "Text Dark Grey": "var(--token-ec2b9b10-d643-4220-9a36-aaf9f4635b2c, rgb(71, 76, 98))",
+    /** #6C6D7A */
+    "TextLight": "var(--token-76a64b2b-abd9-4627-9afb-6ceb52d850d3, rgb(108, 109, 122))",
+    /** #FFFFFF */
+    "White": "var(--token-e84efa84-8a8a-4907-85cb-6027346754f9, rgb(255, 255, 255))",
+    /** #F7F7F7 */
+    "Faint Gray": "var(--token-b616ecc0-8f6b-4bae-84f4-2da14a01e109, rgb(247, 247, 247))",
+})
---
+++ design/framer_files/InesFund_var5.framerfx/code/canvas.tsx
@@ -0,0 +1,19 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Hero = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Label?:string,Label?:string}>(canvas, "id_zQ3F47r7T", {Label:"string",Label:"string"}, 1120,547);
+
+export const colors = Object.freeze({
+    /** #214EB5 */
+    "Enable Blue": "var(--token-c1ea6f7c-d13b-4f4c-b559-449711dea794, rgb(33, 78, 181))",
+    /** #21B549 */
+    "Enable Green": "var(--token-97d6cdea-b672-4647-8d92-6a74b9fe3423, rgb(33, 181, 73))",
+    /** #1E2548 */
+    "TextBlue": "var(--token-510fc827-78f8-4a81-8ee8-82a09c674a44, rgb(30, 37, 72))",
+    /** #7F819F */
+    "TextLight": "var(--token-76a64b2b-abd9-4627-9afb-6ceb52d850d3, rgb(127, 129, 159))",
+    /** #FFFFFF */
+    "White": "var(--token-e84efa84-8a8a-4907-85cb-6027346754f9, rgb(255, 255, 255))",
+    /** #F7F7F7 */
+    "Faint Gray": "var(--token-b616ecc0-8f6b-4bae-84f4-2da14a01e109, rgb(247, 247, 247))",
+})
